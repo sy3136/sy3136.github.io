b9f281ff7b21178aee346644b527dc3bef9858b7
JaeHyeong <dlstngm@naver.com>
2024-03-16 16:49:57 +0900

    init

---
+++ blog-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "Today I learned",
+  description: "공부하고 기록하는 블로그",
+  author: "JaeHyeong",
+  siteUrl: "https://sy3136.github.io",
+  links: {
+    github: "https://github.com/sy3136",
+    etc: "https://blog.naver.com/dlstngm"
+  },
+  utterances: {
+    repo: "sy3136/sy3136.github.io",
+    type: "pathname",
+  },
+}
